8f34ef22960ea3a1cb63de3d60f09a18da5403cd
Abdul Rehman <likeam99@gmail.com>
2024-05-21 18:00:56 +0500

    Building Smallar Carouels

--- khalid-cloths/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ khalid-cloths/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel'
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import { FaChevronLeft } from "react-icons/fa6";
+import { Button } from "@headlessui/react";
 
 const HomeSectionCarousel = () => {
-  return (
-    <div>
-        <AliceCarousel
-        mouseTracking
-        items={items}
-        disableButtonsControls
-        autoPlay
-        autoPlayInterval={1000}
-        infinite
-        controlsStrategy="alternate"
-    />
-    </div>
-  )
-}
+  const responsive = {
+    0: { items: 1 },
+    568: { items: 4 },
+    1024: { items: 6 },
+  };
 
-export default HomeSectionCarousel
+  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+
+  return (
+    <div  className=" relative px-4 lg:px-8 ">
+      <div className=" relative p-5">
+        <AliceCarousel
+          mouseTracking
+          items={items}
+          disableButtonsControls
+          responsive={responsive}
+        />
+        <Button variant="contained"  className=" z-50" sx={{ position:" absolute", top:"8rem", right:"0rem"}} >
+        <FaChevronLeft />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSectionCarousel;
--- khalid-cloths/src/customer/pages/HomePage/HomePage.jsx
+++ khalid-cloths/src/customer/pages/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+import HomeSectionCarousel from '../../components/HomeSectionCarousel/HomeSectionCarousel'
 
 const HomePage = () => {
   return (
     <>
     <MainCarousel />
-    <div>Other SEction</div>
+    <HomeSectionCarousel />
     </>
   )
 }
